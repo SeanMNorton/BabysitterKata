2141976cc01c43d24a2e9057dd99e85f2c134570
Sean Norton <Norton.Sean.M@gmail.com>
2017-11-16 20:17:01 -0600

    private methods tested

--- run.rb
+++ run.rb
@@ -1,9 +1,35 @@
 class SitterCalculator
-  attr_accessor :startTime, :bedTime, :endTime
+  attr_reader :startTime, :bedTime, :endTime
+
   def initialize(startTime, bedTime, endTime)
     @startTime = startTime
     @bedTime = bedTime
     @endTime = endTime
+  end
+
+  def getNightlyCharge
+    return (startToBed + bedToMidnight + midnightToEnd)
+  end
+
+private
+  def startToBed
+    (@bedTime - @startTime) * 12
+  end
+
+  def bedToMidnight
+    if @endTime < 12
+      return (@endTime - @bedTime) * 8
+    else
+      return (12 - @bedTime) * 8
+    end
+  end
+
+  def midnightToEnd
+    if @endTime < 12
+      return 0
+    else
+      (@endTime - 12) * 16
+    end
   end
 
 end
--- tests.rb
+++ tests.rb
@@ -1,5 +1,6 @@
 require './run.rb'
 
+
 RSpec.configure do |config|
   config.tty = true
   config.color = true
@@ -9,10 +10,10 @@ end
 
 describe SitterCalculator do
   before do
-    @calc = SitterCalculator.new(4, 7, 10)
+    @calc = SitterCalculator.new(5, 7, 10)
   end
   it "is initialized with startTime" do
-     expect(@calc.startTime).to eq(4)
+     expect(@calc.startTime).to eq(5)
   end
   it "is initialized with bedTime" do
      expect(@calc.bedTime).to eq(7)
@@ -20,4 +21,31 @@ describe SitterCalculator do
   it "is initialized with endTime" do
      expect(@calc.endTime).to eq(10)
   end
+ describe "getNightlyCharge" do
+   it "returns a number" do
+     expect(@calc.getNightlyCharge).to be_a_kind_of(Integer)
+   end
+   it "returns the correct amount" do
+     expect(@calc.getNightlyCharge).to eq(48)
+   end
+ end
+
+ describe "startToBed" do
+   it "returns dollar amount for startTime to Bedtime charge" do
+     expect(@calc.send(:startToBed)).to eq(24)
+   end
+ end
+
+ describe "bedToMidnight" do
+   it "returns dollar amount for bedTime to midnight charge" do
+     expect(@calc.send(:bedToMidnight)).to eq(24)
+   end
+ end
+
+ describe "midnightToEnd" do
+   it "returns dollar amount for midnight to endtime charge" do
+     expect(@calc.send(:midnightToEnd)).to eq(0)
+   end
+ end
+
 end
